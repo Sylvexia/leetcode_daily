5364b4c98aa22336d7e395a676201a15ff0ff80d
Chartreuse Sylvex <a1085125@mail.nuk.edu.tw>
2022-08-24 15:02:53 +0800

    uwu

---
+++ power_of_three_326.cpp
@@ -0,0 +1,14 @@
+// 326. Power of Three
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPowerOfThree(int n)
+    {
+        return n > 0 && 1162261467 % n == 0;
+    }
+};
