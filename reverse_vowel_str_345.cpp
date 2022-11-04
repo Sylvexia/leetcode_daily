7beed051c57b313643a954bb66e9f38846fb6075
Sylvexya <cuberobin@gmail.com>
2022-11-04 14:38:01 +0800

    darn 中山沒上

---
+++ reverse_vowel_str_345.cpp
@@ -0,0 +1,39 @@
+// 345. Reverse Vowels of a String
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        string vowels="aeiouAEIOU";
+        string str_stack="";
+        for(char ch:s)
+        {
+            for(char vow:vowels)
+            {
+                if(ch==vow)
+                {
+                    str_stack.push_back(ch);
+                    break;
+                }       
+            }
+        }
+
+        for(char& ch:s)
+        {
+            for(char vow:vowels)
+            {
+                if(ch==vow)
+                {
+                    ch=str_stack.back();
+                    str_stack.pop_back();
+                    break;
+                }
+            }
+        }
+
+        return s;
+    }
+};
