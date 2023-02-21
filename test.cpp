47fc21f3be25aa1504c5db71efbbebb1b2173ccb
Sylvexia <cuberobin@gmail.com>
2023-02-22 00:50:08 +0800

    from wsl

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    vector<int> data = {0, 2, 3, 5, 1};
+
+    for (auto ele : data)
+        cout << ele << " ";
+
+    return 0;
+}
