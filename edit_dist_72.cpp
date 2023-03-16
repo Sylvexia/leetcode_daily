e841ebe4efdfbd536fed72e63ba802bb4ea232cc
Sylvexia <cuberobin@gmail.com>
2023-03-16 15:21:36 +0800

    redo edit

--- edit_dist_72.cpp
+++ edit_dist_72.cpp
@@ -36,3 +36,36 @@ public:
         return dfs(word1, word2, 0, 0, dp);
     }
 };
+
+// redo
+class Solution
+{
+    int dfs(string &word1, string &word2, int i, int j,
+            vector<vector<int>> &dp)
+    {
+        if (i == word1.size())
+            return word2.size() - j;
+
+        if (j == word2.size())
+            return word1.size() - i;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (word1[i] == word2[j])
+            return dp[i][j] = dfs(word1, word2, i + 1, j + 1, dp);
+
+        return dp[i][j] = min(
+                              min(dfs(word1, word2, i, j + 1, dp),
+                                  dfs(word1, word2, i + 1, j, dp)),
+                              dfs(word1, word2, i + 1, j + 1, dp)) +
+                          1;
+    }
+
+public:
+    int minDistance(string word1, string word2)
+    {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size(), -1));
+        return dfs(word1, word2, 0, 0, dp);
+    }
+};
