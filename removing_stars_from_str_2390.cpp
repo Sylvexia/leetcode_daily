043e871c4e714476eb43df39c54b7e25561c0e7e
Sylvexia <cuberobin@gmail.com>
2023-04-11 12:39:24 +0800

    wtf this is easy

---
+++ removing_stars_from_str_2390.cpp
@@ -0,0 +1,22 @@
+// 2390. Removing Stars From a String
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string removeStars(string s)
+    {
+        string res;
+        for (char c : s)
+        {
+            if (c == '*')
+                res.pop_back();
+            else
+                res.push_back(c);
+        }
+        return res;
+    }
+};
