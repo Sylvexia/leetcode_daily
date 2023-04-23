5825301d8477bd2d40a59e7b2fb6c53ec1e45d1e
sylvexia <cuberobin@gmail.com>
2023-04-23 13:28:49 +0800

    uwu

---
+++ restore_arr_1416.cpp
@@ -0,0 +1,43 @@
+// 1416. Restore The Array
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+    vector<int> dp;
+    int dfs(int start, string &s, int k)
+    {
+        if (start == s.size())
+            return 1;
+        if (start > s.size())
+            return 0;
+
+        if (s[start] == '0')
+            return 0;
+
+        if (dp[start] != -1)
+            return dp[start];
+
+        int res = 0;
+        long num = 0;
+
+        for (int i = start; i < s.size(); i++)
+        {
+            num = num * 10 + s[i] - '0';
+            if (num > k)
+                break;
+            res += dfs(i + 1, s, k);
+            res %= 1000000007;
+        }
+        return dp[start] = res;
+    }
+
+public:
+    int numberOfArrays(string s, int k)
+    {
+        dp.resize(s.size() + 1, -1);
+        return dfs(0, s, k);
+    }
+};
