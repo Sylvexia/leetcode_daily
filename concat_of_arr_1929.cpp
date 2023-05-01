a8a080f10ecfc77f976d8b84a5a364fc72991671
sylvexia <cuberobin@gmail.com>
2023-05-01 13:54:40 +0800

    meow

---
+++ concat_of_arr_1929.cpp
@@ -0,0 +1,20 @@
+// 1929. Concatenation of Array
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        vector<int> res(nums.size() * 2);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            res[i] = nums[i];
+            res[i + nums.size()] = nums[i];
+        }
+        return res;
+    }
+};
