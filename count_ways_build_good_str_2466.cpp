2b557928432b43eea6973185874ed29dbdafb8fc
sylvexia <cuberobin@gmail.com>
2023-05-13 16:57:24 +0800

    uwu

--- count_ways_build_good_str_2466.cpp
+++ count_ways_build_good_str_2466.cpp
@@ -44,3 +44,30 @@ public:
         return res;
     }
 };
+
+class Solution
+{
+public:
+    int countGoodStrings(int low, int high, int zero, int one)
+    {
+        int n = high + 1;
+        constexpr int mod = 1e9 + 7;
+        int dp[n];
+        memset(dp, 0, sizeof(dp));
+
+        dp[0] = 1;
+        for (int i = 1; i <= high; i++)
+        {
+            if (i >= zero)
+                dp[i] = (dp[i] + dp[i - zero]) % mod;
+            if (i >= one)
+                dp[i] = (dp[i] + dp[i - one]) % mod;
+        }
+
+        int res = 0;
+        for (int i = low; i <= high; i++)
+            res = (res + dp[i]) % mod;
+
+        return res;
+    }
+};
