8cb5c0e2c45f1377df746aaca6e7326b163e4393
sylvexya <cuberobin@gmail.com>
2023-09-07 18:02:08 +0800

    fuck i forget my umbrella

---
+++ decode_str_394.cpp
@@ -0,0 +1,47 @@
+// 394. Decode String
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    string decodeString(string s)
+    {
+        stack<string> chars;
+        stack<int> nums;
+        string res;
+
+        int num = 0;
+        
+        for (char c : s) {
+            if (isdigit(c))
+                num = num * 10 + (c - '0');
+            else if (isalpha(c))
+                res.push_back(c);
+            else if (c == '[') {
+                chars.push(res);
+                nums.push(num);
+                res = "";
+                num = 0;
+            } else if (c == ']') {
+                string temp = res;
+                for (int i = 0; i < nums.top() - 1; i++)
+                    res += temp;
+
+                res = chars.top() + res;
+                chars.pop();
+                nums.pop();
+            }
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    string data{"2[abc]3[cd]ef"};
+    cout << sol.decodeString(data);
+}
