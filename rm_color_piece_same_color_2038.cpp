0eb207ac470f7c355f387bc9e9f4ae6cfebf5aa2
sylvexya <cuberobin@gmail.com>
2023-10-02 10:16:35 +0800

    count>2

---
+++ rm_color_piece_same_color_2038.cpp
@@ -0,0 +1,24 @@
+// 2038. Remove Colored Pieces if Both Neighbors are the Same Color
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool winnerOfGame(string colors)
+    {
+        int alice = 0, bob = 0;
+        for (int i = 2; i < colors.size(); i++) {
+            if (colors[i] == 'A' and colors[i - 1] == 'A' and
+                colors[i - 2] == 'A')
+                alice++;
+            if (colors[i] == 'B' and colors[i - 1] == 'B' and
+                colors[i - 2] == 'B')
+                bob++;
+        }
+
+        return alice>bob;
+    }
+};
