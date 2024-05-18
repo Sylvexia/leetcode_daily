b95f60ce02d7d2b2cb50ce0cf68bbfc73c015bde
sylvexia <cuberobin@gmail.com>
2024-05-18 14:32:01 +0800

    uwu

---
+++ ditri_coin_in_bin_tree_979.cpp
@@ -0,0 +1,38 @@
+// 979. Distribute Coins in Binary Tree
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right)
+    {
+    }
+};
+
+class Solution
+{
+    int res = 0;
+    int dfs(TreeNode *root)
+    {
+        if (!root)
+            return 0;
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+        res += abs(left) + abs(right);
+        return root->val + left + right - 1;
+    }
+
+public:
+    int distributeCoins(TreeNode *root)
+    {
+        dfs(root);
+        return res;
+    }
+};
