07bd1e374a7db5463041466848e413b4013e682f
sylvexia <cuberobin@gmail.com>
2024-07-20 12:23:59 +0800

    uwu:

---
+++ find_val_mtx_row_col_sum_1605.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int speedup = []() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}();
+
+class Solution
+{
+public:
+    vector<vector<int>> restoreMatrix(vector<int> &rowSum, vector<int> &colSum)
+    {
+        int m = rowSum.size();
+        int n = colSum.size();
+        vector<vector<int>> res(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] -= res[i][j];
+                colSum[j] -= res[i][j];
+            }
+        }
+        return res;
+    }
+};
