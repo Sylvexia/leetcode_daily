0102767b8e07c6415755ad53b04257d154c05317
sylvexia <cuberobin@gmail.com>
2024-11-25 23:48:30 +0800

    i suck

---
+++ slide_puzzle_773.cpp
@@ -0,0 +1,63 @@
+// 773. Sliding Puzzle
+
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+static int speedup = []() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}();
+
+class Solution
+{
+public:
+    int slidingPuzzle(vector<vector<int>> &board)
+    {
+        vector<vector<int>> directions = {{1, 3}, {0, 2, 4}, {1, 5},
+                                          {0, 4}, {1, 3, 5}, {2, 4}};
+
+        string target = "123450";
+        string startState;
+
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                startState += to_string(board[i][j]);
+            }
+        }
+
+        unordered_set<string> visited;
+        queue<string> queue;
+        queue.push(startState);
+        visited.insert(startState);
+
+        int moves = 0;
+
+        while (!queue.empty()) {
+            int size = queue.size();
+            while (size-- > 0) {
+                string currentState = queue.front();
+                queue.pop();
+
+                if (currentState == target) {
+                    return moves;
+                }
+
+                int zeroPos = currentState.find('0');
+                for (int newPos : directions[zeroPos]) {
+                    string nextState = currentState;
+                    swap(nextState[zeroPos], nextState[newPos]);
+
+                    if (visited.count(nextState))
+                        continue;
+
+                    visited.insert(nextState);
+                    queue.push(nextState);
+                }
+            }
+            moves++;
+        }
+        return -1;
+    }
+};
