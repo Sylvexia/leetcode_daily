86e85cd442db9a3568617e5487dea46ecad2a263
sylvexia <cuberobin@gmail.com>
2026-05-04 02:11:26 +0800

    uwu

---
+++ strictly_palindromic_num_2396.cpp
@@ -0,0 +1,17 @@
+// 3760. Maximum Substrings With Distinct Start
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int speedup = []() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}();
+
+class Solution {
+public:
+    bool isStrictlyPalindromic(int n) {
+        return false;
+    }
+};
