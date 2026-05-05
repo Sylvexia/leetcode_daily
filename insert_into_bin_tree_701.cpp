1f8bc2d29ca9bdf9658f44443de06259958525a6
sylvexia <cuberobin@gmail.com>
2026-05-06 07:54:44 +0800

    uwu

---
+++ insert_into_bin_tree_701.cpp
@@ -0,0 +1,32 @@
+// 701. Insert into a Binary Search Tree
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right)
+    {
+    }
+};
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution
+{
+public:
+    TreeNode *insertIntoBST(TreeNode *root, int val)
+    {
+        if (!root)
+            return new TreeNode(val);
+        if (val < root->val)
+            root->left = insertIntoBST(root->left, val);
+        else
+            root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
