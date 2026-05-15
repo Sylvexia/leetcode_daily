84d4d0e2f5021837d0f56a0bdded641bd7a1a06c
sylvexia <cuberobin@gmail.com>
2026-05-15 22:25:07 +0800

    maxxing

---
+++ convert_bin_num_in_linked_list_to_int_1290.cpp
@@ -0,0 +1,32 @@
+// 1290. Convert Binary Number in a Linked List to Integer
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+static int speedup = []() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}();
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    int getDecimalValue(ListNode *head)
+    {
+        int res = 0;
+        for (auto node = head; node; node = node->next) {
+            res *= 2;
+            res += node->val;
+        }
+        return res;
+    }
+};
