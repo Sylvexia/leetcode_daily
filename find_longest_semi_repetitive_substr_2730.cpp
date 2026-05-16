1b9564d1a84baad3868396c77dabad01b3847367
sylvexia <cuberobin@gmail.com>
2026-05-17 04:11:38 +0800

    greed

--- find_longest_semi_repetitive_substr_2730.cpp
+++ find_longest_semi_repetitive_substr_2730.cpp
@@ -15,19 +15,16 @@ public:
     int longestSemiRepetitiveSubstring(string s)
     {
         int n = s.size();
-        int r{};
-        int res{0};
-        int cnt{0};
-        for (int l = 0; l < n; l++) {
-            for (; r < n and cnt <= 1; r++) {
-                if (r > 0 and s[r] == s[r - 1])
-                    cnt++;
-                if (cnt > 1)
-                    break;
+        int l = 0;
+        int res = 1;
+        int cnt = 0;
+        for (int r = 1; r < n; r++) {
+            cnt += (s[r] == s[r - 1]);
+            for (; cnt > 1; l++) {
+                if (s[l] == s[l + 1])
+                    cnt--;
             }
-            res = max(res, r - l);
-            if (l + 1 < n and s[l] == s[l + 1])
-                cnt--;
+            res = max(res, r - l + 1);
         }
         return res;
     }
