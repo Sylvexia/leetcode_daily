c105f22db4938f6b7cc2b3caafa94f455f1292bd
sylvexia <cuberobin@gmail.com>
2026-05-27 08:29:39 +0800

    uwu

---
+++ count_the_num_of_special_char_ii_3121.cpp
@@ -0,0 +1,27 @@
+// 3121. Count the Number of Special Characters II
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+
+static int speedup = []() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}();
+
+class Solution
+{
+public:
+    int numberOfSpecialChars(string word)
+    {
+        bitset<26> lo, hi;
+        for (char ch : word) {
+            if (islower(ch))
+                lo[ch - 'a'] = !hi[ch - 'a'];
+            else
+                hi[ch - 'A'] = true;
+        }
+        return (lo & hi).count();
+    }
+};
