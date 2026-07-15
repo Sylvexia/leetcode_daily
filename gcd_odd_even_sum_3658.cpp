6739196f37728b0046c46a045ca33331e2486fe2
Sylvexia <cuberobin@gmail.com>
2026-07-15 08:47:55 +0800

    uwu

---
+++ gcd_odd_even_sum_3658.cpp
@@ -0,0 +1,20 @@
+// 3658. GCD of Odd and Even Sums
+
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+static int speedup = []() {
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    return 0;
+}();
+
+class Solution
+{
+public:
+    int gcdOfOddEvenSums(int n)
+    {
+        return n;
+    }
+};
